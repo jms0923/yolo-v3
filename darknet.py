70f2efbac58c9b6bd276b4d65e4f3ed965aa8587
JMS <jms588@naver.com>
2020-03-26 16:49:57 +0900

    add module

--- darknet.py
+++ darknet.py
@@ -8,8 +8,6 @@ import numpy as np
 
 
 
-
-
 def parse_cfg(cfgFile):
     '''
     parse cfg file and divide into block as dict form
@@ -45,4 +43,57 @@ def create_modules(blocks):
     module_list = nn.ModuleList()
     prev_filters = 3
     output_fliters = []
-    
+
+    for index, x in enumerate(blocks[1:]):
+        # 모듈을 순차적으로 실행 시킴
+        module = nn.Sequential()
+
+        if (x["type"] == "convolutional"):
+            # layer 정보 받아옴
+            activation = x["actiavtion"]
+            try:
+                batch_normalize = int(x["batch_normalize"])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+        
+            filters = int(x["filters"])
+            padding = int(x["pad"])
+            kernel_size = int(x["size"])
+            stride = int(x["stride"])
+
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+
+            # convolutional layer 추가
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+            module.add_module()
+
+            # batch norm layer 추가
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+
+            # activation을 확인
+            # yolo는 linear or leaky ReLU
+            if activation == "leaky":
+                activn = nn.LeakyReLU(0.1, inplace=True)
+                module.add_module("leaky_{0}".format(index), activn)
+                            
+        # 만약 upsampling layer면
+        # Bilinear2dUpsampling을 사용
+        elif (x["type"] == "upsample"):
+            stride = int(x["stride"])
+            upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+            module.add_module("upsample_{}".format(index), upsample)
+
+
+
+
+
+
+
+
